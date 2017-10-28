25bb96198867f8c23f18b96420359102bf80c039
Tyler Alves <tyleralves@Tylers-MacBook-Pro.local>
2017-10-28 00:57:32 -0700

    Improve git shell script for tracking annotation line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  include Response
+  def index
+
+      rout, wout = IO.pipe
+      rerr, werr = IO.pipe
+
+      pid = spawn("./clone.sh", :out => wout, :err => werr)
+      _, status = Process.wait2(pid)
+
+      wout.close
+      werr.close
+
+      output = rout.readlines
+      .map{|item|
+        item[/^\w+\s(\d+)/, 1]
+      }
+      .join(', ')
+
+      json_response({status:status, output:output})
+    end
 end
--- clone.sh
+++ clone.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-echo 'testscript'
-mkdir temp_clone && cd temp_clone
-git clone --bare --depth 10 https://github.com/tyleralves/volunteerApp.git
+mkdir temp_clone
+cd temp_clone
+# Just for testing, REMOVE!
+rm -rf volunteerApp.git
+# When user makes their first annotation
+git clone --bare --depth 30 https://github.com/tyleralves/volunteerApp.git &> /dev/null
+git pull &> /dev/null
 cd volunteerApp.git
-git blame master -- app.js
+startCommit=$(git log --pretty=format:%H | tail -1)
+git blame -L 1,4 -n --reverse $startCommit -- app.js
+# BEGIN removing past history
+git fetch --depth 1
+git reflog expire --expire-unreachable=now --all
+git gc --aggressive --prune=all
+# END remove past history
+# Local history only contains HEAD
 exit 2
+
+
+# TODO: Extract blame line number and
